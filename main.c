f5931c4ffbf96363b67b75d15ca78dacf4eb928d
Cooper Lala <clala@mi-v3.msk.21-school.ru>
2021-01-26 19:06:37 +0300

    print_entry

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
-int		main(void)
+#include "minishell.h"
+
+int			print_entry(void)
 {
-	
+	ft_printf("$> ");
+	return (1);
+}
+
+int			main(void)
+{
+	char	*s;
+
+	while (print_entry() && get_next_line(STDIN_FILENO, &s))
+	{
+		;
+	}
 	return (0);
 }
