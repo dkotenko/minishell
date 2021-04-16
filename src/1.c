1fa8759c2d260e5c8facffe878e6037c5a494341
dkotenko <dmitrii.kotenko@gmail.com>
2021-04-16 17:57:51 +0300

    hello gnl

--- src/1.c
+++ src/1.c
@@ -1,18 +1,25 @@
 #include "libft.h"
+#include <stdlib.h>
 
 int main()
 {
-	int i = 0;
+
 	char *s;
+	
+	ft_printf("got: %s\n", "hello");
+	
 	while (get_next_line(STDIN_FILENO, &s))
 	{
 		//if (ft_strequ(s, "exit"))
 		//	exit(0);
+		ft_printf("got: %s\n", s);
 		
-		if (++i == 3)
-			break ;
+		
 		free(s);
+		//if (++i == 3)
+		//	break ;
 		
 	}
 	exit(0);
+	
 }
